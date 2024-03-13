4216e4b5c47c354cb91acf0b3aa2d13d901f22ef
parrTh11 <162285049+parrTh11@users.noreply.github.com>
2024-03-13 16:31:43 +0530

    Microsoft Unity Added

--- MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -4,22 +4,23 @@ using System.Linq;
 using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
+using MyShop.Core;
 using MyShop.Core.Models;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
-        string className; 
+        string className;
 
         public InMemoryRepository()
         {
             className = typeof(T).Name;
             items = cache[className] as List<T>;
 
-            if(items == null)
+            if (items == null)
             {
                 items = new List<T>();
             }
@@ -29,7 +30,7 @@ namespace MyShop.DataAccess.InMemory
         {
             cache[className] = items;
         }
-        
+
         public void Insert(T t)
         {
             items.Add(t);
@@ -39,7 +40,7 @@ namespace MyShop.DataAccess.InMemory
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
 
-            if(tToUpdate != null)
+            if (tToUpdate != null)
             {
                 tToUpdate = t;
             }
@@ -52,7 +53,7 @@ namespace MyShop.DataAccess.InMemory
         public T Find(string Id)
         {
             T t = items.Find(i => i.Id == Id);
-            if( t != null)
+            if (t != null)
             {
                 return t;
             }
